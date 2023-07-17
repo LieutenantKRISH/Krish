b556e029b90d1bc445a824660786a75cf5ece5b8
LieutenantKRISH <136014092+LieutenantKRISH@users.noreply.github.com>
2023-07-17 17:42:50 +0530

    Update and rename README.md to cuboid dimension.py

---
+++ cuboid dimension.py
@@ -0,0 +1,52 @@
+ list of all possible coordinates within a given cuboid dimensions, excluding coordinates where the sum of the three values is equal to a given number.
+the cod efor this
+
+if _ _name_ _ == '_ _main_ _':
+    x = int(input(""))
+    y = int(input(""))
+    z = int(input(""))
+    n = int(input(""))
+    coordinates = []
+    for i in range(x+1):
+      for j in range(y+1):
+        for k in range(z+1):
+          if i+j+k != n:
+            coordinates.append([i,j,k])
+    print(coordinates)
+    
+EXPLANATION:-
+(1)   if __name__ == '__main__':
+
+This line checks if the code is being run as the main program and not imported as a module. It ensures that the following code block will only execute if the file is being run directly.
+
+(2)   x = int(input()), y = int(input()), z = int(input()), n = int(input()):
+
+These lines prompt the user to enter four integer values for x, y, z, and n. The input() function reads the user's input as a string, and the int() function is used to convert the string input into integer values.
+
+
+(3)   coordinates = []:
+
+This line initializes an empty list called coordinates where we will store the valid coordinates that satisfy the condition.
+
+
+(4)   Nested Loops:
+
+The code then enters three nested loops using for statements:
+for i in range(x + 1) iterates over the values from 0 to x.
+for j in range(y + 1) iterates over the values from 0 to y.
+for k in range(z + 1) iterates over the values from 0 to z.
+These loops cover all possible combinations of values for i, j, and k within the given dimensions x, y, and z.
+
+
+(5)   Condition Check:
+
+Within the nested loops, the code checks the condition if i + j + k != n.
+If the sum of i, j, and k is not equal to n, it means the current combination of coordinates satisfies the condition.
+In this case, [i, j, k] is appended to the coordinates list using coordinates.append([i, j, k]).
+
+
+(5)   Print Result:
+
+After the nested loops complete execution, the list coordinates contains all the valid coordinates that meet the condition.
+Finally, the code prints the coordinates list using print(coordinates).
+          
