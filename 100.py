0a9cce26df226513852a27eabaac99f688302acf
LieutenantKRISH <136014092+LieutenantKRISH@users.noreply.github.com>
2023-07-27 15:20:59 +0530

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,19 +1,19 @@
-(Q1) Write a program which will find all such numbers which are divisible by 7 but
+"""(Q1) Write a program which will find all such numbers which are divisible by 7 but
 are not a multiple of 5,
 between 2000 and 3200 (both included).
 The numbers obtained should be printed in a comma-separated sequence on a
 single line.
-(ANS) 
+(ANS) """
 l = []
 for i in range(2000,3201):
     if(i%7 == 0)and (i%5 != 0):
         l.append(str(i))
 print (','.join(l))
 
-(Q2) Write a program which can compute the factorial of a given numbers.
+"""(Q2) Write a program which can compute the factorial of a given numbers.
 The results should be printed in a comma-separated sequence on a single line.
 
-(ANS) 
+(ANS) """
 def fact(x):
     if x == 0:
         return 1
